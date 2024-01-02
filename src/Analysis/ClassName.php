eec130b61c4e7f7c23f5b3d575972381884c6b97
Marcus Kober <marcus.kober@tetraeder.com>
2024-01-02 22:32:51 +0100

    DOC Add comments

--- src/Analysis/ClassName.php
+++ src/Analysis/ClassName.php
@@ -6,13 +6,25 @@ namespace Marcus\PhpLegacyAnalyzer\Analysis;
 
 use PhpParser\Node;
 
+/**
+ * ClassName ValueObject
+ *
+ * Creates a ClassName out of a PHPParser Node while detecting anonymous classes.
+ */
 class ClassName
 {
+    /**
+     * @return string
+     */
     public function __toString(): string
     {
         return $this->name;
     }
 
+    /**
+     * @param Node $node
+     * @return ClassName
+     */
     public static function ofNode(Node $node): ClassName
     {
         $name = $node->namespacedName ?? 'anonymous@' . spl_object_hash($node);
@@ -20,6 +32,11 @@ class ClassName
         return new self((string) $name);
     }
 
+    /**
+     * Private constructor
+     *
+     * @param string $name
+     */
     private function __construct(private string $name)
     {
 
