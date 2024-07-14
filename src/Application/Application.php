d0a8e4985688217ebaf5431bc5fe29c9e740a037
Marcus Kober <marcus.kober@tetraeder.com>
2024-07-14 17:06:51 +0200

    MOD Remove composer plugin

--- src/Application/Application.php
+++ src/Application/Application.php
@@ -37,7 +37,7 @@ use Twig\Loader\FilesystemLoader;
 
 final readonly class Application
 {
-    const VERSION = '0.3.8';
+    const VERSION = '0.3.10';
 
     /**
      * @throws ConfigFileExtensionNotSupportedException
